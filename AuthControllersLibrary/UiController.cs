01e8e74b2d29c2e242e312fb6cf366540ac260c2
o.smirnov <oleg.smirnov@firstlinesoftware.com>
2019-08-06 22:15:57 +0300

    New ui controller

---
+++ AuthControllersLibrary/UiController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthControllersLibrary
+{
+    public class UiController : ControllerBase
+    {
+
+        [Route("GetAds")]
+        [HttpPost]
+        public IActionResult GetAds()
+        {
+            return new OkObjectResult(new { Ok = "Ok" });
+        }
+
+    }
+}
